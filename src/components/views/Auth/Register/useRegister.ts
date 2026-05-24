8d6715017923063e64babfde65022ed351865f09
Miowme <williamsusantolukman@gmail.com>
2026-05-24 15:36:48 +0700

    feat: User Auth Activation

--- src/components/views/Auth/Register/useRegister.ts
+++ src/components/views/Auth/Register/useRegister.ts
@@ -3,7 +3,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { IRegister } from "@/types/Auth";
-import authServices from "@/services/auth";
+import authServices from "@/services/auth.service";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 
