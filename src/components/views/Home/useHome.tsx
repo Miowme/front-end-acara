386b4da11064f592e4bf4ab2d3336fc787012b77
Miowme <williamsusantolukman@gmail.com>
2026-06-13 16:24:36 +0700

    feat: homepage

--- src/components/views/Home/useHome.tsx
+++ src/components/views/Home/useHome.tsx
@@ -1,5 +1,7 @@
-import { LIMIT_BANNER, PAGE_DEFAULT } from "@/constants/list.constants";
+import { LIMIT_BANNER, LIMIT_CATEGORY, LIMIT_EVENT, PAGE_DEFAULT } from "@/constants/list.constants";
 import bannerServices from "@/services/banner.service";
+import categoryServices from "@/services/category.service";
+import eventServices from "@/services/event.service";
 import { useQuery } from "@tanstack/react-query";
 
 
@@ -16,13 +18,63 @@ const useHome = () => {
         isLoading: isLoadingBanners,
     } = useQuery({
         queryKey: ["Banners"],
-        queryFn: () => getBanners(),
+        queryFn: getBanners,
+        enabled: true,
+    });
+
+    const getCategories = async () => {
+        let params = `limit=${LIMIT_CATEGORY}&page=${PAGE_DEFAULT}`;
+        const res = await categoryServices.getCategories(params);
+        const { data } = res;
+        return data;
+    };
+
+    const {
+        data: dataCategories,
+        isLoading: isLoadingCategories,
+    } = useQuery({
+        queryKey: ["Categories"],
+        queryFn: getCategories,
+        enabled: true,
+    });
+
+    const getEvents = async (params: string) => {
+        const res = await eventServices.getEvents(params);
+        const { data } = res;
+        return data;
+    };
+
+    const currentEventQuery = `limit=${LIMIT_EVENT}&page=${PAGE_DEFAULT}&isPublished=true`
+
+    const {
+        data: dataFeaturedEvents,
+        isLoading: isLoadingFeaturedEvents,
+    } = useQuery({
+        queryKey: ["FeaturedEvents"],
+        queryFn: () => getEvents(
+            `${currentEventQuery}&isFeatured=true`
+        ),
+        enabled: true,
+    });
+
+    const {
+        data: dataLatestEvents,
+        isLoading: isLoadingDataLatestEvents,
+    } = useQuery({
+        queryKey: ["LatestEvents"],
+        queryFn: () => getEvents(currentEventQuery),
         enabled: true,
     });
 
     return {
         dataBanners,
         isLoadingBanners,
+        dataFeaturedEvents,
+        isLoadingFeaturedEvents,
+        dataLatestEvents,
+        isLoadingDataLatestEvents,
+        dataCategories,
+        isLoadingCategories,
     };
 }
 
